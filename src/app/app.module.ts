7482f04d8d5d070b80962a6a0cfc7b3c0132eeee
RaSegistan <rafaelsegistanxib@gmail.com>
2018-07-15 22:11:11 -0500

    added movie service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchbarService } from './searchbar.service';
 import { FormsModule } from '@angular/forms';
+import { MoviesService } from './movies.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule, AppRoutingModule, FormsModule],
-  providers: [SearchbarService],
+  providers: [SearchbarService, MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Movie } from 'src/app/global/models/Movie.model';
 import { SearchbarService } from '../../searchbar.service';
+import { MoviesService } from '../../movies.service';
 
 @Component({
   selector: 'app-movies',
@@ -11,34 +12,20 @@ export class MoviesComponent implements OnInit, OnDestroy {
   movies: Movie[] = [];
   searchText: string;
 
-  public constructor(private searchService: SearchbarService) {
+  public constructor(
+    private searchService: SearchbarService,
+    private movieService: MoviesService
+  ) {
     this.searchService.searchText.subscribe(val => {
       this.searchText = val;
     });
   }
 
   public ngOnInit(): void {
-    this.loadMovies();
+    this.movies = this.movieService.getAll();
   }
 
   ngOnDestroy(): void {
     this.searchService.searchText.unsubscribe();
-  }
-
-  loadMovies() {
-    const n = 10;
-
-    for (let i = 0; i < n; i++) {
-      this.movies.push(
-        new Movie(
-          i,
-          'movie' + i,
-          [],
-          '',
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/7dAh80ydoWvUaBE8uFYkp9WsoSC.jpg',
-          ''
-        )
-      );
-    }
   }
 }
